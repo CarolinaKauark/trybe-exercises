2d302e551f0d647cf12a4a248380ee4eb6c8cac0
Ckauark <ckauark@gmail.com>
2022-03-09 14:51:23 -0300

    1, 2 e 3 exercicio de logica

---
+++ Trybe-exercises/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/2-exercicio/2.js
@@ -0,0 +1,9 @@
+let word = 'tryber';
+let inverse = '';
+
+for(let i = word.length - 1 ; i >= 0; i--){
+    inverse += word[i];
+}
+
+console.log(inverse);
+
---
+++ Trybe-exercises/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/3-exercicio/3.js
@@ -0,0 +1,18 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = '';
+let menor = array[0];
+
+for(let i = 0; i < array.length; i++){
+    if(array[i].length > maior.length){
+        maior = array[i];
+    }
+}
+
+for(let i = 1; i < array.length; i++){
+    if(array[i].length < menor.length){
+        menor = array[i];
+    }
+}
+
+console.log(maior);
+console.log(menor);
