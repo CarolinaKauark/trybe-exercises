997a8067969cb8e11005aeb1fa2db850f4cd5d94
Ckauark <ckauark@gmail.com>
2022-03-24 19:01:33 -0300

    exercicio bloco 5.3

--- Trybe-exercises/Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ Trybe-exercises/Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -18,7 +18,7 @@ function createDaysOfTheMonth(){
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     const month = document.querySelector('#days');
 
-    for (let index = 0; index < dezDaysList.length; index += 1){
+    for (let index = 0; index < dezDaysList.length; index += 1) {
         const day = dezDaysList[index];
         const dayLi = document.createElement('li');
                 
@@ -96,6 +96,90 @@ const fridayList = [4, 11, 18, 25];
 sextou(fridayList);
 
 //Exercício 6
+function mouseOver () {
+    let monthDay = document.querySelector('#days');
+
+    monthDay.addEventListener('mouseover', function(event) {
+        event.target.style.fontSize = '30px';
+        event.target.style.fontWeight = '600';
+
+    })
+};
+
+function mouseOut () {
+    let monthDay = document.querySelector('#days');
+
+    monthDay.addEventListener('mouseout', function(event) {
+        event.target.style.fontSize = '20px';
+        event.target.style.fontWeight = '200';
+
+    })
+};
+
+mouseOver();
+mouseOut();
+
+
+//Exercício 7
+function tarefaPersonalizada (nomeTarefa) {
+    const tarefa = document.createElement('span');
+    tarefa.innerHTML = nomeTarefa;
+    document.querySelector('.my-tasks').appendChild(tarefa);
+};
+
+tarefaPersonalizada('Cozinhar');
+
+//Exercício  8 
+function corTarefa(cor){
+    const tarefaDiv = document.createElement('div');
+    tarefaDiv.style.backgroundColor = cor;
+    tarefaDiv.classList = 'task';
+    document.querySelector('.my-tasks').appendChild(tarefaDiv);
+}
+
+corTarefa('green');
+
+//Exercício 9
+function selecionarTarefa() {
+    let tarefa = document.querySelector('.task');
+    
+    tarefa.addEventListener('click', function() {
+        if (tarefa.classList == 'task') {
+            tarefa.classList.add('selected');
+        } else if (tarefa.classList == 'task selected'){
+            tarefa.classList.remove('selected');
+        }
+    } )
+}
+
+selecionarTarefa();
+
+
+//Exercício 10
+
+function tarefaCalendario() {
+    let monthDay = document.querySelector('#days');
+    const tarefaSelecionada = document.getElementsByClassName('selected'); //pq não consigo usar o querySelectoraAll('.selected') ?
+    let colorTask = document.querySelector('.task').style.backgroundColor;
+
+    monthDay.addEventListener('click', function(event){
+        //essa primeira condição não funciona com uso o querySelectorAll lá na const tarefaSelecionada
+        if (tarefaSelecionada.length > 0){ 
+            if (event.target.style.color == colorTask) {
+                event.target.style.color = 'rgb(119,119,119)';
+           } else {
+               event.target.style.color = colorTask;
+           }
+        }
+    });
+
+}
+
+tarefaCalendario();
+
+//bonus
+
+
 
 
 
