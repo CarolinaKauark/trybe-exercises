0b894d5768af6f5d13e1456ed842685c9531f79d
Ckauark <ckauark@gmail.com>
2022-04-01 19:11:18 -0300

    ParteI .2

--- Trybe-exercises/Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteI/script.js
+++ Trybe-exercises/Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteI/script.js
@@ -11,19 +11,29 @@
 
 //   testingScope(true);
 
-  const testingScope = escopo => {
-    if (escopo === true) {
-        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-        ifScope = `${ifScope}. ótimo, fui utilizada no escopo !`;
-        console.log(ifScope);
-      } else {
-        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-        console.log(elseScope);
-      }
-  };
+// const testingScope = escopo => {
+// if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope}. ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//     } else {
+//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//     }
+// };
 
-  testingScope(true);
+// testingScope(true);
 
 // const testingScope = (escopo) => ( escopo === true ? console.log(`Não devo ser utilizada fora do meu escopo (if)`) : console.log(`Não devo ser utilizada fora meu escopo (else)`));
 
 // testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort(function(a,b) {
+        return a-b;
+    });
+
+// Seu código aqui.
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
