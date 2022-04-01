3c7ed649ea485d3dd98b0c82c3c659ad1afa1c50
Ckauark <ckauark@gmail.com>
2022-04-01 18:57:56 -0300

    Iniciando os exercícios do dia 7.1

---
+++ Trybe-exercises/Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteI/script.js
@@ -0,0 +1,29 @@
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+// //   }
+
+//   testingScope(true);
+
+  const testingScope = escopo => {
+    if (escopo === true) {
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = `${ifScope}. ótimo, fui utilizada no escopo !`;
+        console.log(ifScope);
+      } else {
+        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+      }
+  };
+
+  testingScope(true);
+
+// const testingScope = (escopo) => ( escopo === true ? console.log(`Não devo ser utilizada fora do meu escopo (if)`) : console.log(`Não devo ser utilizada fora meu escopo (else)`));
+
+// testingScope(true);
