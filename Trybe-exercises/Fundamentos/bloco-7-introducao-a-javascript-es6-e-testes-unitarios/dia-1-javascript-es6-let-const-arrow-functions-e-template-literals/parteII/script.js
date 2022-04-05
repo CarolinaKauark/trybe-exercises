0d90ff7b8ec66a5d77bcc2a7fbb850c69f50cccf
Ckauark <ckauark@gmail.com>
2022-04-05 10:35:28 -0300

    Finalizando exercícios do dia 7.1

--- Trybe-exercises/Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteII/script.js
+++ Trybe-exercises/Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parteII/script.js
@@ -1,12 +1,14 @@
 //1.Crie uma função que receba um número e retorne seu fatorial.
 
-const fatorial = (num) => {
-    let fat = 1;
-    for (let index = num; index > 0; index -= 1) {
-        fat *= index;
-    }
-    return `O fatorial de ${num} é ${fat}!`;
-}
+// const fatorial = (num) => {
+//     let fat = 1;
+//     for (let index = num; index > 0; index -= 1) {
+//         fat *= index;
+//     }
+//     return `O fatorial de ${num} é ${fat}!`;
+// }
+
+const fatorial = (n) => n !== 0 ? (n* fatorial(n-1)) : 1;
 
 console.log(fatorial(4));
 
@@ -28,10 +30,26 @@ console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu
 
 //  3.Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
 
-const btn = document.getElementsByTagName('button')[0];
-let counter = document.getElementsByTagName('h1')[0];
+// const btn = document.getElementsByTagName('button')[0];
+// let counter = document.getElementsByTagName('h1')[0];
 
-let clickCounter = 0;
+// let clickCounter = 0;
 
-btn.addEventListener('click', () => counter.innerHTML = clickCounter +=1);
+// btn.addEventListener('click', () => counter.innerHTML = clickCounter +=1);
 
+const skills = ['html', 'css', 'js', 'react', 'git'];
+
+const xChange = (str) => {
+    const frase = `Tryber ${str} aqui!`;
+    const fraseSkills = (frase) => {
+        let fraseCompleta = frase;
+        skills.forEach((currentElement, index) => {
+            fraseCompleta += ` 
+            ${currentElement}`;
+        });
+        return fraseCompleta
+    }
+    return fraseSkills(frase);
+}
+
+console.log(xChange('bebeto'));
